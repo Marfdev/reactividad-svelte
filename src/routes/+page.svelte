3c32dee48af9459f542297ceb2e09dbf350b938f
Marfdev <mrondon820@gmail.com>
2024-04-22 06:25:38 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
+    $: evenOrOdd = count % 2 === 0 ? 'even' : 'odd';
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+<span>
+    count is {evenOrOdd}
+</span>
